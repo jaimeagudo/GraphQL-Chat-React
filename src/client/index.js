cb16e6d5aa79f6ea4d4d9deba0f0ec104e2a1b3a
jaimeagudo <jaimeagudo@users.no.reply.github.com>
2018-07-27 11:03:28 +0200

    Revert "to revert"
    This reverts commit b5be9fd73ed7a1d1c28f77af28aae49e508e3c13.

--- src/client/index.js
+++ src/client/index.js
@@ -1,23 +1,15 @@
-import ApolloClient, { createNetworkInterface } from "apollo-client";
-import {
-  SubscriptionClient,
-  addGraphQLSubscriptions
-} from "subscriptions-transport-ws";
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
+import { SubscriptionClient, addGraphQLSubscriptions } from 'subscriptions-transport-ws';
 
-// const wsClient = new SubscriptionClient('ws://localhost:4010/subscriptions');
-const wsClient = new SubscriptionClient("ws://localhost:8888/graphql-ws");
-const uri = "http://localhost:8888/graphql";
+const wsClient = new SubscriptionClient('ws://localhost:4010/subscriptions');
+
 const baseNetworkInterface = createNetworkInterface({
-  uri
-  //  uri: '/graphql',
+  uri: '/graphql',
 });
 
-const subscriptionNetworkInterface = addGraphQLSubscriptions(
-  baseNetworkInterface,
-  wsClient
-);
+const subscriptionNetworkInterface = addGraphQLSubscriptions(baseNetworkInterface, wsClient);
 
 export default new ApolloClient({
   networkInterface: subscriptionNetworkInterface,
-  connectToDevTools: true
+  connectToDevTools: true,
 });
