b5be9fd73ed7a1d1c28f77af28aae49e508e3c13
jaimeagudo <jaimeagudo@users.no.reply.github.com>
2018-07-27 11:03:18 +0200

    to revert

--- src/client/index.js
+++ src/client/index.js
@@ -1,15 +1,23 @@
-import ApolloClient, { createNetworkInterface } from 'apollo-client';
-import { SubscriptionClient, addGraphQLSubscriptions } from 'subscriptions-transport-ws';
+import ApolloClient, { createNetworkInterface } from "apollo-client";
+import {
+  SubscriptionClient,
+  addGraphQLSubscriptions
+} from "subscriptions-transport-ws";
 
-const wsClient = new SubscriptionClient('ws://localhost:4010/subscriptions');
-
+// const wsClient = new SubscriptionClient('ws://localhost:4010/subscriptions');
+const wsClient = new SubscriptionClient("ws://localhost:8888/graphql-ws");
+const uri = "http://localhost:8888/graphql";
 const baseNetworkInterface = createNetworkInterface({
-  uri: '/graphql',
+  uri
+  //  uri: '/graphql',
 });
 
-const subscriptionNetworkInterface = addGraphQLSubscriptions(baseNetworkInterface, wsClient);
+const subscriptionNetworkInterface = addGraphQLSubscriptions(
+  baseNetworkInterface,
+  wsClient
+);
 
 export default new ApolloClient({
   networkInterface: subscriptionNetworkInterface,
-  connectToDevTools: true,
+  connectToDevTools: true
 });
